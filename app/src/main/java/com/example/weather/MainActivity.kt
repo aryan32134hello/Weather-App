48e17669d0ad6396001504a253e520a98a5ed0c3
aryan32134hello <aryan32134@gmail.com>
2022-12-23 01:08:37 +0530

    first commit

---
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -0,0 +1,95 @@
+package com.example.weather
+
+import android.app.Dialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.io.BufferedReader
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
+import java.io.IOException
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.SocketTimeoutException
+import java.net.URL
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        lifecycleScope.launch {
+            Log.i("JSON", callApiLogin())
+        }
+    }
+
+    private lateinit var customProgressDialog: Dialog
+
+    //Todo 5 initialize and show dialog
+    private fun showProgressDialog() {
+        customProgressDialog = Dialog(this)
+        customProgressDialog.setContentView(R.layout.dialog_custom_progress)
+        customProgressDialog.show()
+    }
+
+    //Todo 6 cancel dialog
+    private fun cancelDialog() {
+        customProgressDialog.dismiss()
+    }
+
+    //Todo 7 create a suspend function for the network call
+    suspend fun callApiLogin(): String = withContext(Dispatchers.IO) {
+        var result = ""
+        //Todo 8: show the dialog on ui thread
+        runOnUiThread {
+            showProgressDialog()
+        }
+        var connection: HttpURLConnection? = null
+        try {
+            val url = URL("https://run.mocky.io/v3/e1b48409-8bbc-459c-973d-1b72c56da455")
+            connection = url.openConnection() as HttpURLConnection
+            connection.doInput = true
+            connection.doOutput = true
+            val httpResult: Int = connection.responseCode
+            if (httpResult == HttpURLConnection.HTTP_OK) {
+                val inputStream = connection.inputStream
+                val reader = BufferedReader(InputStreamReader(inputStream))
+                val stringBuilder = StringBuilder()
+                var line: String?
+                try {
+                    while (reader.readLine().also { line = it } != null) {
+                        stringBuilder.append(line + "\n")
+
+                    }
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                } finally {
+                    try {
+                        inputStream.close()
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+                }
+                result = stringBuilder.toString()
+            } else {
+                result = connection.responseMessage
+            }
+            //Todo 9: cancel the Dialog on ui thread
+            runOnUiThread {
+                cancelDialog()
+            }
+
+        } catch (e: SocketTimeoutException) {
+            result = "Connection Timeout"
+        } catch (e: IOException) {
+            result = "Error: ${e.message}"
+        } finally {
+            connection?.disconnect()
+        }
+        result
+    }.toString()
+
+}
+
